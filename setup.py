9725fe92c4e309d22f7f15066a9a5342e3e629f0
samreay <samuelreay@gmail.com>
2017-11-27 12:04:59 +1000

    Linking requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='WorkshopExample',
       version='0.0.1',
       description='Random example project for coding workshop',
@@ -7,5 +10,6 @@ setup(name='WorkshopExample',
       author='Samuel Hinton',
       author_email='samuelreay@gmail.com',
       license='MIT',
+      install_requires=requirements,
       packages=find_packages(exclude=('tests', 'doc'))
       )
