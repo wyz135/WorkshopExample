df39c227b0faaac6411994ea967de3029c909969
samreay <samuelreay@gmail.com>
2017-11-27 11:40:29 +1000

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='WorkshopExample',
+      version='0.0.1',
+      description='Random example project for coding workshop',
+      url='http://github.com/Samreay/WorkshopExample',
+      author='Samuel Hinton',
+      author_email='samuelreay@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=('tests', 'doc'))
+      )
