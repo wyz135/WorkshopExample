9d1fce3f5964af686a5c489e0009d5fcc2ccc464
Samuel Hinton <samuelreay@gmail.com>
2017-12-01 19:01:47 +1000

    Reducing amount of bad code to fix

--- some_module/code.py
+++ some_module/code.py
@@ -1,5 +1,6 @@
 # So here is some just amazing code. It is for making plots of various things from the 6dF survey
 # and calculating overdensity at particular coords
+# If you have time in the exercise, try to get this fixed up, efficient, tested and documented
 import numpy as np
 
 f = open(r"C:\Users\shint1\PyCharmProjects\WorkshopExample\data\6df.csv", 'r')
@@ -29,50 +30,6 @@ mag_b = np.array(mag_b)
 mag_r = np.array(mag_r)
 print("Have %d entries" % len(ra))
 
-import matplotlib.pyplot as plt
-fig, axes = plt.subplots(2, 2, figsize=(7,7))
-axes[0, 0].scatter(z, mag_b, c=a_v, s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[0, 0].set_title("z vs mag_b")
-axes[0, 0].set_xlabel("z")
-axes[0, 0].set_ylabel("mag_b")
-axes[1, 0].scatter(z, mag_r, c=a_v, s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[1, 0].set_title("z vs mag_r")
-axes[1, 0].set_xlabel("z")
-axes[1, 0].set_ylabel("mag_r")
-axes[1, 1].scatter(mag_r, mag_b, c=a_v, s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[1, 1].set_title("mag_r vs mag_b")
-axes[1, 1].set_xlabel("mag_r")
-axes[1, 1].set_ylabel("mag_b")
-axes[0, 1].scatter(z, mag_b - mag_r, c=a_v, s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[0, 1].set_title("z vs mag_b - mag_r")
-axes[0, 1].set_xlabel("z")
-axes[0, 1].set_ylabel("mag_b - mag_r")
-plt.tight_layout()
-plt.show()
-fig, axes = plt.subplots(2, 2, figsize=(7,7))
-mask = (z < 0.2) & (mag_b > 10)
-axes[0, 0].scatter(z[mask], mag_b[mask], c=a_v[mask], s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[0, 0].set_title("z vs mag_b")
-axes[0, 0].set_xlabel("z")
-axes[0, 0].set_ylabel("mag_b")
-axes[1, 0].scatter(z[mask], mag_r[mask], c=a_v[mask], s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[1, 0].set_title("z vs mag_r")
-axes[1, 0].set_xlabel("z")
-axes[1, 0].set_ylabel("mag_r")
-axes[1, 1].scatter(mag_r[mask], mag_b[mask], c=a_v[mask], s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[1, 1].set_title("mag_r vs mag_b")
-axes[1, 1].set_xlabel("mag_r")
-axes[1, 1].set_ylabel("mag_b")
-axes[0, 1].scatter(z[mask], mag_b[mask] - mag_r[mask], c=a_v[mask], s=3, edgecolor='none', cmap='jet', alpha=0.5)
-axes[0, 1].set_title("z vs mag_b - mag_r")
-axes[0, 1].set_xlabel("z")
-axes[0, 1].set_ylabel("mag_b - mag_r")
-plt.tight_layout()
-plt.show()
-# calculate num points close to a ra/dec
-if False:
-    plt.scatter(ra, dec)
-    plt.show()
 radius = 1
 ra_sn = [50, 75, 175, 200, 205, 300]
 dec_sn = [-40, -60, -10, -30, -5, -70]
